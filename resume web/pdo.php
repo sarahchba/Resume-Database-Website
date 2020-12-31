68e35e4707197d4718d846a3f9ad3561a9bff646
sarahchba <75021464+sarahchba@users.noreply.github.com>
2020-12-31 11:00:57 +0000

    Update pdo.php

--- resume web/pdo.php
+++ resume web/pdo.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=resume', 'sarah', 'zap');
+$pdo = new PDO('mysql:host=localhost;port=3;dbname='resume', 'sarah', 'one');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
